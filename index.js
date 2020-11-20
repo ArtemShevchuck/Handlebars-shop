22a4d3d98be0c645d63a4c8fd5f66817ee1bc398
shalynishka225 <shalynishka225@gmail.com>
2020-11-20 05:16:57 +0200

    initialization

---
+++ index.js
@@ -0,0 +1,94 @@
+const express = require("express");
+const Handlebars = require("handlebars");
+const path = require("path");
+const csurf = require("csurf");
+const flash = require("connect-flash");
+const mongoose = require("mongoose");
+const exphbs = require("express-handlebars");
+const session = require("express-session");
+const MongoStore = require("connect-mongodb-session")(session);
+const homeRoutes = require("./routes/home");
+const cardRoutes = require("./routes/card");
+const addRoutes = require("./routes/add");
+const coursesRoutes = require("./routes/courses");
+const ordersRoutes = require("./routes/orders");
+const profileRoutes = require("./routes/profile");
+const varMiddleware = require("./middleware/variables");
+const userMiddleware = require("./middleware/user");
+const fileMiddleware = require("./middleware/file");
+const authRoutes = require("./routes/auth");
+const errorHandler = require("./middleware/error");
+const keys = require("./keys");
+const helmet = require("helmet");
+const compression = require("compression");
+const {
+  allowInsecurePrototypeAccess,
+} = require("@handlebars/allow-prototype-access");
+
+const app = express();
+
+const store = new MongoStore({
+  collection: "sessions",
+  uri: keys.MONGODB_URI,
+});
+
+app.engine(
+  "hbs",
+  exphbs({
+    defaultLayout: "main",
+    extname: "hbs",
+    helpers: require("./utils/hbs-helper"),
+    handlebars: allowInsecurePrototypeAccess(Handlebars),
+  })
+);
+app.set("view engine", "hbs");
+app.set("views", "views");
+
+app.use(express.static(path.join(__dirname, "public")));
+app.use("/images", express.static(path.join(__dirname, "images")));
+app.use(express.urlencoded({ extended: true }));
+app.use(
+  session({
+    secret: keys.SESSION_SECRET,
+    resave: false,
+    saveUninitialized: false,
+    store,
+  })
+);
+app.use(fileMiddleware.single("avatar"));
+app.use(csurf());
+app.use(flash());
+//app.use(helmet());
+app.use(compression());
+app.use(varMiddleware);
+app.use(userMiddleware);
+
+app.use("/", homeRoutes);
+app.use("/add", addRoutes);
+app.use("/courses", coursesRoutes);
+app.use("/card", cardRoutes);
+app.use("/orders", ordersRoutes);
+app.use("/auth", authRoutes);
+app.use("/profile", profileRoutes);
+
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 3002;
+
+async function start() {
+  try {
+    await mongoose.connect(keys.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      useFindAndModify: false,
+    });
+
+    app.listen(PORT, () => {
+      console.log(`Server is running on ${PORT}`);
+    });
+  } catch (e) {
+    console.log(e);
+  }
+}
+
+start();
